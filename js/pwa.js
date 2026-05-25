6136bb0233306011951969bbc8bf1d8676bf7957
IvanRAviles <ivanraviles13@protonmail.com>
2026-05-24 18:37:31 -0700

    Delete js directory

--- js/pwa.js
+++
@@ -1,7 +0,0 @@
-export function registerSW() {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('./sw.js').then(r => r.update()).catch(console.error);
-    });
-  }
-}
